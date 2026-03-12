a54e598bfc9d3281f832fcda7edb00de0abd3d32
ANA RAQUEL <dograubiquel@gmail.com>
2026-03-12 14:32:49 -0300

    Create mario kart-Node.js

---
+++ prompt/mario kart-Node.js
@@ -0,0 +1,14 @@
+https://github.com/seu-usuario/mario-kart-simulator
+Como executar o projeto
+
+Clone o repositório:
+
+git clone https://github.com/seu-usuario/mario-kart-simulator.git
+
+Acesse a pasta do projeto:
+
+cd mario-kart-simulator
+
+Execute o simulador:
+
+node index.js
